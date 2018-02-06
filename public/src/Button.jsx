2d42081419a0e00f1dfe1cb652e9b8a17dbaf30a
KeithWang <570109400@qq.com>
2018-02-06 13:49:06 +0800

    君心

--- public/src/Button.jsx
+++ public/src/Button.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 export class Button extends React.Component {
     render() {
         return (
-            <div>按钮</div>
+            <button type="button">按钮</button>
         );
     }
 }
